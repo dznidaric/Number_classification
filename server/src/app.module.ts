df3b768d20585350096dbe4b87d149ad8e077911
dznidaric <61658892+dznidaric@users.noreply.github.com>
2023-02-16 23:47:26 +0100

    postavljanje BE, FE i modela za klasifikaciju

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClassificationModule } from './classification/classification.module';
 
 @Module({
-  imports: [],
+  imports: [ClassificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
